fe1e0f7d484a6b8c8c83251191501435fca81832
KAWNINJ6 <74711750+KAWNINJ6@users.noreply.github.com>
2021-04-22 20:06:51 +0200

    Test passé (pour une utilisation basique du pion) càd : avancé de "1" verticalement, de "2" verticalement seulement une seule fois

--- Tests/pieces/PionTest.java
+++ Tests/pieces/PionTest.java
@@ -11,15 +11,14 @@ class PionTest {
         Pion pion = new Pion(0,0,Couleur.BLANC);
         assertTrue(pion.estPossible(2,0));
         assertTrue(pion.estPossible(1,0));
-//        assertTrue(pion.estPossible(1,1));
-//        assertTrue(pion.estPossible(1, -1));
+//        assertTrue(pion.estPossible(1,1)); (test = si on prends en compte la prise en diagonale)
+//        assertTrue(pion.estPossible(1, -1)); (test = si on prends en compte la prise en diagonale)
         assertFalse(pion.estPossible(5, 2));
 
         pion.newPos(0,0);
-        assertFalse(pion.estPossible(0,2));
-//        assertTrue(pion.estPossible(1,1));
-//        assertTrue(pion.estPossible(-1, 1));
-        assertTrue(pion.estPossible(1,1));
-
+        assertFalse(pion.estPossible(2,0));
+        assertTrue(pion.estPossible(1,0));
+//        assertTrue(pion.estPossible(1,1)); (test = si on prends en compte la prise en diagonale)
+//        assertTrue(pion.estPossible(-1, 1)); (test = si on prends en compte la prise en diagonale)
     }
 }
