a9e418a1b74a34de43d53734322ebc3d8a2a4041
Yan2708 <74718439+Yan2708@users.noreply.github.com>
2021-04-22 18:14:21 +0200

    Commentaires + test

--- Tests/pieces/PionTest.java
+++ Tests/pieces/PionTest.java
@@ -11,15 +11,15 @@ class PionTest {
         Pion pion = new Pion(0,0,Couleur.BLANC);
         assertTrue(pion.estPossible(2,0));
         assertTrue(pion.estPossible(1,0));
-        assertTrue(pion.estPossible(1,1));
-        assertTrue(pion.estPossible(1, -1));
+//        assertTrue(pion.estPossible(1,1));
+//        assertTrue(pion.estPossible(1, -1));
         assertFalse(pion.estPossible(5, 2));
 
         pion.newPos(0,0);
 
         assertFalse(pion.estPossible(0,2));
-        assertTrue(pion.estPossible(1,1));
-        assertTrue(pion.estPossible(-1, 1));
+//        assertTrue(pion.estPossible(1,1));
+//        assertTrue(pion.estPossible(-1, 1));
         assertTrue(pion.estPossible(1,1));
 
     }
