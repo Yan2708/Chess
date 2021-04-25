863eed7a90e0f0d242f5b56f0bbcd7149cb8afc3
Yan2708 <74718439+Yan2708@users.noreply.github.com>
2021-04-25 10:15:33 +0200

    test diagonales

--- Tests/echequier/ReglesTest.java
+++ Tests/echequier/ReglesTest.java
@@ -12,14 +12,20 @@ class ReglesTest {
 
     @Test
     void testCheck(){
-        Echiquier e = new Echiquier(new FabriquePiece(), "r6T/T7/8/8/8/8/8/8");
+        Echiquier e = new Echiquier(new FabriquePiece(), "r6D/D7/8/8/8/8/8/8");
         ArrayList<IPiece> pieces = Regle.getPieceFromColor(e, "NOIR");
         assertTrue(Regle.check(e,new Coord(0,0),e.getPiece(new Coord(0,7))));
-        assertTrue(Regle.check(e,new Coord(0,0),e.getPiece(new Coord(2,0))));
+        assertTrue(Regle.check(e,new Coord(0,0),e.getPiece(new Coord(1,0))));
         assertFalse( Regle.checkMate(e, "BLANC", new Coord(0,0)));
-        e.deplacer(2,0,7,0);
+        e.deplacer(1,0,7,0);
+
+        System.out.println( e.getPiece(new Coord(7,0)));
         assertTrue(Regle.check(e,new Coord(0,0),e.getPiece(new Coord(7,0))));
         assertTrue(Regle.checkMate(e, "BLANC", new Coord(0,0)));
+        e.deplacer(7,0,7,7);
+        System.out.println( e.getPiece(new Coord(7,7)));
+        assertTrue(Regle.check(e,new Coord(0,0),e.getPiece(new Coord(7,7))));
+        assertFalse(Regle.checkMate(e, "BLANC", new Coord(0,0)));
 
     }
 }
